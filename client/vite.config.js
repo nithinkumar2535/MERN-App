575350c658b8e663d1bd3da124155ff30bdf8813
nithinkumar2535 <163820998+nithinkumar2535@users.noreply.github.com>
2024-06-06 13:33:48 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api/' : `${import.meta.env.VITE_SERVER_URL}`
+      '/api/' :  "http://localhost:3000"
     }
   },
   plugins: [react()],
